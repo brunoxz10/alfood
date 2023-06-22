189db14198aeae21240c8d9fe004d01d0386e111
Bruno Henrique Bento <bruno_henrique300@hotmail.com>
2023-06-22 07:54:53 -0300

    consumo api pratos e inicio admin restaurantes

--- src/componentes/ListaRestaurantes/Restaurante/index.tsx
+++ src/componentes/ListaRestaurantes/Restaurante/index.tsx
@@ -1,12 +1,30 @@
+import { useEffect, useState } from 'react';
 import IRestaurante from '../../../interfaces/IRestaurante';
 import Prato from '../Prato';
 import estilos from './Restaurante.module.scss';
+import IPrato from '../../../interfaces/IPrato';
+import axios from 'axios';
 
 interface RestauranteProps {
   restaurante: IRestaurante
 }
 
 const Restaurante = ({ restaurante }: RestauranteProps) => {
+
+  const [pratos, setPratos] = useState<IPrato[]>([]);
+
+  useEffect(() => {
+    // Obter pratos do restaurante
+    axios.get<IPrato[]>(`http://localhost:8000/api/v1/restaurantes/${restaurante.id}/pratos/`)
+      .then(response => {
+        setPratos(response.data)
+      })
+      .catch(erro => {
+        console.log(erro)
+      })
+  }, [restaurante.id]);
+
+  restaurante.pratos = pratos;
 
   return (<section className={estilos.Restaurante}>
     <div className={estilos.Titulo}>
