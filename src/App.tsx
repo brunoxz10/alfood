1e3d0b4181253562c6053389a25548cca7c6f623
Bruno Henrique Bento <bruno_henrique300@hotmail.com>
2023-06-26 07:51:19 -0300

    implementacao crud de pratos

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import AdministracaoRestaurantes from './paginas/Administracao/Restaurantes';
 import FormularioRestaurante from './paginas/Administracao/Restaurantes/FormularioRestaurante';
 import PaginaBaseAdmin from './paginas/Administracao/PaginaBaseAdmin';
 import AdministracaoPratos from './paginas/Administracao/Pratos';
+import FormularioPrato from './paginas/Administracao/Pratos/FormularioPrato';
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
         <Route path="restaurantes/novo" element={<FormularioRestaurante />} />
         <Route path="restaurantes/:id" element={<FormularioRestaurante />} />
         <Route path="pratos" element={<AdministracaoPratos />} />
+        <Route path="pratos/novo" element={<FormularioPrato />} />
       </Route>
     </Routes>
   );
